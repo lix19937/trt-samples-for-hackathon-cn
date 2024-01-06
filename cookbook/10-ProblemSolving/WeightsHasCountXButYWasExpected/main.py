499654ed2164e978ac043083f1d75e5fe9757892
lix19937 <38753233+lix19937@users.noreply.github.com>
2024-01-06 11:31:18 +0800

    Update main.py

--- cookbook/10-ProblemSolving/WeightsHasCountXButYWasExpected/main.py
+++ cookbook/10-ProblemSolving/WeightsHasCountXButYWasExpected/main.py
@@ -18,15 +18,19 @@ import numpy as np
 import tensorrt as trt
 
 kernel = np.ones([32, 3, 5, 5], dtype=np.float32)  # count of weight is incorrect
-bias = np.ones(7, dtype=np.float32)
+bias = np.ones(7, dtype=np.float32) # fp32  
 
+# 构建器 网络 config   
 logger = trt.Logger(trt.Logger.ERROR)
 builder = trt.Builder(logger)
 network = builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
 config = builder.create_builder_config()
-inputT0 = network.add_input("inputT0", trt.float32, (1, 3, 600, 800))
 
+# 搭建网络  
+inputT0 = network.add_input("inputT0", trt.float32, (1, 3, 600, 800))  
 convolutionLayer = network.add_convolution_nd(inputT0, 32, [5, 5], kernel, bias)
 
 network.mark_output(convolutionLayer.get_output(0))
+
+# 序列化引擎  
 engineString = builder.build_serialized_network(network, config)
