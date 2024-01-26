0af2a67e2fa6f123c1e88748daa939cf7dde74af
lix19937 <38753233+lix19937@users.noreply.github.com>
2024-01-26 15:10:33 +0800

    Update main.cpp

--- cookbook/01-SimpleDemo/TensorRT8.5/main.cpp
+++ cookbook/01-SimpleDemo/TensorRT8.5/main.cpp
@@ -22,22 +22,19 @@ using namespace nvinfer1;
 const std::string trtFile {"./model.plan"};
 static Logger     gLogger(ILogger::Severity::kERROR);
 
-void run()
-{
+void run(){
     ICudaEngine *engine = nullptr;
 
-    if (access(trtFile.c_str(), F_OK) == 0)
-    {
+    // 如果有 plan 文件  
+    if (access(trtFile.c_str(), F_OK) == 0) {
         std::ifstream engineFile(trtFile, std::ios::binary);
         long int      fsize = 0;
-
         engineFile.seekg(0, engineFile.end);
         fsize = engineFile.tellg();
         engineFile.seekg(0, engineFile.beg);
         std::vector<char> engineString(fsize);
         engineFile.read(engineString.data(), fsize);
-        if (engineString.size() == 0)
-        {
+        if (engineString.size() == 0){
             std::cout << "Failed getting serialized engine!" << std::endl;
             return;
         }
@@ -45,55 +42,53 @@ void run()
 
         IRuntime *runtime {createInferRuntime(gLogger)};
         engine = runtime->deserializeCudaEngine(engineString.data(), fsize);
-        if (engine == nullptr)
-        {
+        if (engine == nullptr){
             std::cout << "Failed loading engine!" << std::endl;
             return;
         }
         std::cout << "Succeeded loading engine!" << std::endl;
     }
-    else
-    {
+    else {  // 没有plan,没有onnx  用户自己使用trt api搭建网络，  从这里我们可以看到完整过程   
         IBuilder             *builder = createInferBuilder(gLogger);
         INetworkDefinition   *network = builder->createNetworkV2(1U << int(NetworkDefinitionCreationFlag::kEXPLICIT_BATCH));
         IOptimizationProfile *profile = builder->createOptimizationProfile();
         IBuilderConfig       *config  = builder->createBuilderConfig();
         config->setMemoryPoolLimit(MemoryPoolType::kWORKSPACE, 1 << 30);
 
+        // 搭建网络 
         ITensor *inputTensor = network->addInput("inputT0", DataType::kFLOAT, Dims32 {3, {-1, -1, -1}});
         profile->setDimensions(inputTensor->getName(), OptProfileSelector::kMIN, Dims32 {3, {1, 1, 1}});
         profile->setDimensions(inputTensor->getName(), OptProfileSelector::kOPT, Dims32 {3, {3, 4, 5}});
         profile->setDimensions(inputTensor->getName(), OptProfileSelector::kMAX, Dims32 {3, {6, 8, 10}});
         config->addOptimizationProfile(profile);
-
         IIdentityLayer *identityLayer = network->addIdentity(*inputTensor);
         network->markOutput(*identityLayer->getOutput(0));
+
+        // 序列化网络  
         IHostMemory *engineString = builder->buildSerializedNetwork(*network, *config);
-        if (engineString == nullptr || engineString->size() == 0)
-        {
+        if (engineString == nullptr || engineString->size() == 0){
             std::cout << "Failed building serialized engine!" << std::endl;
             return;
         }
         std::cout << "Succeeded building serialized engine!" << std::endl;
 
+        // 反序列化网络  
         IRuntime *runtime {createInferRuntime(gLogger)};
         engine = runtime->deserializeCudaEngine(engineString->data(), engineString->size());
-        if (engine == nullptr)
-        {
+        if (engine == nullptr){
             std::cout << "Failed building engine!" << std::endl;
             return;
         }
         std::cout << "Succeeded building engine!" << std::endl;
 
+        // 存储plan 到本地 
         std::ofstream engineFile(trtFile, std::ios::binary);
-        if (!engineFile)
-        {
+        if (!engineFile){
             std::cout << "Failed opening file to write" << std::endl;
             return;
         }
         engineFile.write(static_cast<char *>(engineString->data()), engineString->size());
-        if (engineFile.fail())
-        {
+        if (engineFile.fail()){
             std::cout << "Failed saving .plan file!" << std::endl;
             return;
         }
@@ -101,21 +96,23 @@ void run()
     }
 
     long unsigned int        nIO     = engine->getNbIOTensors();
-    long unsigned int        nInput  = 0;
-    long unsigned int        nOutput = 0;
+    long unsigned int        nInput  = 0, nOutput = 0;
     std::vector<std::string> vTensorName(nIO);
-    for (int i = 0; i < nIO; ++i)
-    {
+    // 统计 in out tensor数目 
+    for (int i = 0; i < nIO; ++i){
         vTensorName[i] = std::string(engine->getIOTensorName(i));
         nInput += int(engine->getTensorIOMode(vTensorName[i].c_str()) == TensorIOMode::kINPUT);
         nOutput += int(engine->getTensorIOMode(vTensorName[i].c_str()) == TensorIOMode::kOUTPUT);
     }
 
-    IExecutionContext *context = engine->createExecutionContext();
+    // 创建上下文  
+    IExecutionContext *context = engine->createExecutionContext();  
+
+    // 设置 输入shape 
     context->setInputShape(vTensorName[0].c_str(), Dims32 {3, {3, 4, 5}});
 
-    for (int i = 0; i < nIO; ++i)
-    {
+    // 打印tensor属性  
+    for (int i = 0; i < nIO; ++i) {
         std::cout << std::string(i < nInput ? "Input [" : "Output[");
         std::cout << i << std::string("]-> ");
         std::cout << dataTypeToString(engine->getTensorDataType(vTensorName[i].c_str())) << std::string(" ");
@@ -125,66 +122,63 @@ void run()
     }
 
     std::vector<int> vTensorSize(nIO, 0);
-    for (int i = 0; i < nIO; ++i)
-    {
+    // 计算每一个tensor的size 以字节数为单位   
+    for (int i = 0; i < nIO; ++i) {
         Dims32 dim  = context->getTensorShape(vTensorName[i].c_str());
         int    size = 1;
-        for (int j = 0; j < dim.nbDims; ++j)
-        {
+        for (int j = 0; j < dim.nbDims; ++j){
             size *= dim.d[j];
         }
         vTensorSize[i] = size * dataTypeToSize(engine->getTensorDataType(vTensorName[i].c_str()));
     }
 
-    std::vector<void *>
-                        vBufferH {nIO, nullptr};
-    std::vector<void *> vBufferD {nIO, nullptr};
-    for (int i = 0; i < nIO; ++i)
-    {
+    // 给每一个tensor分配空间  
+    std::vector<void *> vBufferH {nIO, nullptr}, vBufferD {nIO, nullptr};
+    for (int i = 0; i < nIO; ++i) {
         vBufferH[i] = (void *)new char[vTensorSize[i]];
         CHECK(cudaMalloc(&vBufferD[i], vTensorSize[i]));
     }
 
-    float *pData = (float *)vBufferH[0];
-
-    for (int i = 0; i < vTensorSize[0] / dataTypeToSize(engine->getTensorDataType(vTensorName[0].c_str())); ++i)
-    {
+    // 给输入tensor赋值  实际情况可能就是输入图像   
+    auto pData = (float *)vBufferH[0];
+    for (int i = 0; i < vTensorSize[0] / dataTypeToSize(engine->getTensorDataType(vTensorName[0].c_str())); ++i) {
         pData[i] = float(i);
     }
-    for (int i = 0; i < nInput; ++i)
-    {
+    // 主机到设备  
+    for (int i = 0; i < nInput; ++i) {
         CHECK(cudaMemcpy(vBufferD[i], vBufferH[i], vTensorSize[i], cudaMemcpyHostToDevice));
     }
 
-    for (int i = 0; i < nIO; ++i)
-    {
+    // 绑定tensor地址   
+    for (int i = 0; i < nIO; ++i) {
         context->setTensorAddress(vTensorName[i].c_str(), vBufferD[i]);
     }
 
+    // 推理执行  
     context->enqueueV3(0);
 
-    for (int i = nInput; i < nIO; ++i)
-    {
+    // 设备到主机  
+    for (int i = nInput; i < nIO; ++i) {
         CHECK(cudaMemcpy(vBufferH[i], vBufferD[i], vTensorSize[i], cudaMemcpyDeviceToHost));
     }
 
-    for (int i = 0; i < nIO; ++i)
-    {
+    // 打印结果  
+    for (int i = 0; i < nIO; ++i) {
         printArrayInformation((float *)vBufferH[i], context->getTensorShape(vTensorName[i].c_str()), vTensorName[i], true, true);
     }
 
-    for (int i = 0; i < nIO; ++i)
-    {
+    // 资源释放  
+    for (int i = 0; i < nIO; ++i) {
         delete[] (char *)vBufferH[i];
         CHECK(cudaFree(vBufferD[i]));
     }
-    return;
 }
 
-int main()
-{
+int main(){
     CHECK(cudaSetDevice(0));
+    // 1次热身    
     run();
+    
     run();
     return 0;
 }
